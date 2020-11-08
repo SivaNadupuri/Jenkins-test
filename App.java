a6f2c90894953a8d77ac7d751ec1601fe6249e8f
SivaNadupuri <40888140+SivaNadupuri@users.noreply.github.com>
2020-11-08 16:42:16 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App{
     public static void main(String...args){
-           System.out.println("Hello Jenkins to github");
+           System.out.println("Hello Jenkins to github1");
                   }
             }
      
